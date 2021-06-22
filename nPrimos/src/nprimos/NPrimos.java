a8f6261f0e62cf6b779d44bcb5380f31e52cda69
Leonardo <profeleo@gmail.com>
2021-06-22 15:44:13 -0500

    se agregaron comentarios

--- nPrimos/src/nprimos/NPrimos.java
+++ nPrimos/src/nprimos/NPrimos.java
@@ -2,7 +2,7 @@
  * PROGRAMA PARA SABER SI UN NUMERO ES PRIMO O NO
  */
 package nprimos;
-import java.util.*;
+import java.util.*;     // libreria para el Scanner
 /**
  *
  * @author leooosan
@@ -13,14 +13,17 @@ public class NPrimos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        int numero;
+        Scanner entrada = new Scanner(System.in);   // para leer datos por consola
+        int numero;     // numero para evaluar si es primo
         int j;
         System.out.println("ESCRIBA UN NUMERO: ");
         numero = entrada.nextInt();
+        
+        // codigo principal
         j=2;
         while(numero % j != 0)
             ++j;
+        
         if(j == numero) System.out.println("EL NUMERO ES PRIMO");
         else System.out.println("EL NUMERO NO ES PRIMO");
     }
